6b42cf66d716fa0ecdd88ffcce7a7e9d051de460
Sai Chittella <saichittella30@gmail.com>
2024-03-18 20:25:02 -0700

    Navbar Clean

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,16 +1,16 @@
 const NavBar = () => {
 	return (
 		<nav
-			className="fixed flex justify-between items-center px-10 py-3 w-full rounded-b-lg"
-			style={{ borderBottom: "1px solid #A9A9A9" }}
+			className="fixed flex justify-between items-center px-14 py-4 w-full rounded-b-lg"
+			style={{ borderBottom: "1px solid #222222" }}
 		>
 			<div>
-				<a href="/#home" className="font-bold">
+				<a href="/#home" className="font-bold text-lg px-16">
 					Alpine Dev
 				</a>
 			</div>
 
-			<div className="flex items-center space-x-16 pr-8">
+			<div className="flex items-center space-x-20 pr-10">
 				<a href="/#mission" className="nav-item">
 					Mission
 				</a>

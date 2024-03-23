f9d41ca459b4580a504e93e57295ec9ac1693dbd
Semi-C0de <lalithg@outlook.com>
2024-03-22 23:27:16 -0700

    removed 'our mission' from grid system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,32 @@
 export default function Home() {
 	return (
-		<div className="grid grid-rows-3 h-screen">
+		<>
+			<div className="grid grid-rows-3 h-screen">
 
-			{/* Title */}
-			<div className="row-start-2 text-center text-H1 leading-tight ">
-				<span className="font-medium">Reaching new heights with a</span>
-				<span className="block font-extrabold bg-gradient-to-r from-[#126F4C] to-[#9CD298] text-transparent bg-clip-text">
-					website for you
-				</span>
+				{/* Title */}
+				<div className="row-start-2 text-center text-H1 leading-tight ">
+					<span className="font-medium">Reaching new heights with a</span>
+					<span className="block font-extrabold bg-gradient-to-r from-[#126F4C] to-[#9CD298] text-transparent bg-clip-text">
+						website for you
+					</span>
+				</div>
+
+				{/* Button */}
+				<div className="row-start-3 text-center">
+					<a href="#contact" className="py-2 px-5 bg-[#3A6436] rounded-full text-H4 font-light"
+						style={{
+							animation:
+								"glow-animation 2s ease-out infinite alternate",
+						}}>
+						Contact Us
+					</a>
+				</div>
 			</div>
-
-			{/* Button */}
-			<div className="row-start-3 text-center">
-				<a href="#contact" className="py-2 px-5 bg-[#3A6436] rounded-full text-H4 font-light"
-					style={{
-						animation:
-							"glow-animation 2s ease-out infinite alternate",
-					}}>
-					Contact Us
-				</a>
-			</div>
-
 			<div>
 				<p className="text-center text-4xl text-green-500 font-bold">
 					Our Mission
 				</p>
 			</div>
-		</div>
+		</ >
 	);
 }

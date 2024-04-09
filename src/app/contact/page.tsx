78786edb47ad30c31037f0b96e81ed6e2eb3d53e
Sai Chittella <saichittella30@gmail.com>
2024-04-08 18:01:58 -0700

    Updated Contact Us Page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import Textbox from "@/components/Textbox";
-import Link from "next/link";
+import { validateHeaderValue } from "http";
 import React, { useState } from "react";
 
 export default function ContactUs() {
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
 	const [message, setMessage] = useState("");
+	const arrow = "/imgs/arrow.png";
 
 	const handleNameChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
 		setName(e.target.value);
@@ -21,99 +22,130 @@ export default function ContactUs() {
 	};
 
 	const handleSubmit = () => {
-        // We will need to change this; this is just a placeholder for right now, we need to make it so that it takes all this data and then sends it to like our email or something
+		// We will need to change this; this is just a placeholder for right now, we need to make it so that it takes all this data and then sends it to like our email or something
 		console.log("Submitted!", name, email, message);
 	};
 
 	return (
 		<div>
-			<div>
+			<div className="max-w-full text-center">
 				<p
-					className="max-w-full text-center font-bold"
+					style={{
+						position: "absolute",
+						top: "calc(100vh / 11.3)",
+						left: "44%",
+						fontSize: "19px",
+					}}
+				>
+					Level up your brand!
+				</p>
+				<p
+					className="font-bold"
 					style={{
 						color: "#1CB47C",
-						fontSize: "74px",
+						fontSize: "56px",
 						position: "absolute",
-						top: "calc(100vh / 19)",
+						top: "calc(100vh / 10)",
 						left: "50%",
 						transform: "translateX(-50%)",
 					}}
 				>
-					Contact Us!
+					Contact Us
 				</p>
 			</div>
-
 			<div
-				className="absolute bg-[#0a5c0228]"
+				className="absolute bg-[#152115]"
 				style={{
-					width: "775px",
-					height: "792px",
-					top: "calc(100vh / 6)",
-					left: "23%",
+					width: "800px",
+					height: "570px",
+					top: "calc(100vh / 3.5)",
+					left: "42%",
 					borderRadius: "3%",
 				}}
 			/>
-
+			<Textbox
+				divTop="calc(100vh / 3.2)"
+				divLeft="4%"
+				inputText="Name"
+				placeHolder="Your Name"
+				inputWidth="400px"
+				inputHeight="50px"
+				value={name}
+				onChange={handleNameChange}
+			/>
+			<Textbox
+				divTop="calc(100vh / 2.25)"
+				divLeft="4%"
+				inputText="Email"
+				placeHolder="you@company.com"
+				inputWidth="400px"
+				inputHeight="50px"
+				value={email}
+				onChange={handleEmailChange}
+			/>
+			<Textbox
+				divTop="calc(100vh / 1.75)"
+				divLeft="4%"
+				inputText="How can we help?"
+				placeHolder="Tell us about the project..."
+				inputWidth="400px"
+				inputHeight="227px"
+				value={message}
+				onChange={handleMessageChange}
+			/>
 			<div
 				style={{
 					position: "absolute",
-					top: "165px",
-					left: "377px",
+					top: "calc(100vh / 1.13)",
+					left: "4%",
+					fontSize: "15px",
 				}}
 			>
-				<p className="font-bold" style={{ fontSize: "25px" }}>
-					Level up your brand!
-				</p>
-				<p className="py-1" style={{ fontSize: "20px" }}>
-					You can reach us at any time via{" "}
+				<p>
+					Here is our email, just in case:{" "}
 					<span className="text-[#1CB47C]">
 						idk_example@gmail.com
 					</span>
 				</p>
 			</div>
-
-			<Textbox
-				divTop="270px"
-				divLeft="471px"
-				inputText="First and Last name"
-				placeHolder="Your name"
-				inputWidth="532px"
-				inputHeight="64px"
-				value={name}
-				onChange={handleNameChange}
-			/>
-			<Textbox
-				divTop="400px"
-				divLeft="471px"
-				inputText="Email Address"
-				placeHolder="you@company.com"
-				inputWidth="532px"
-				inputHeight="64px"
-				value={email}
-				onChange={handleEmailChange}
-			/>
-			<Textbox
-				divTop="530px"
-				divLeft="471px"
-				inputText="How can we help?"
-				placeHolder="Tell us about your project..."
-				inputWidth="532px"
-				inputHeight="226px"
-				value={message}
-				onChange={handleMessageChange}
-			/>
-
 			<button
-				className="py-2 px-5 bg-[#3A6436] rounded-full text-H4 font-light"
+				className="py-1.5 px-9 bg-[#3A6436] rounded-full text-H4 font-bold"
 				onClick={handleSubmit}
 				style={{
 					position: "absolute",
-					top: "840px",
-					left: "650px",
+					top: "calc(100vh / 1.08)",
+					left: "4%",
+					fontSize: "18px",
 				}}
 			>
-				Submit
+				Send
 			</button>
+
+			<div
+				className="pb-5"
+				style={{
+					position: "absolute",
+					top: "calc(100vh / 0.95)",
+					left: "25%",
+					fontSize: "16px",
+				}}
+			>
+				<p>
+					Here are some of our projects... Let's put{" "}
+					<span className="text-[#1CB47C]">your's</span> on here!
+				</p>
+				<img
+					src={arrow}
+					alt=""
+					height="100px"
+					width="100px"
+					style={{
+						position: "absolute",
+						left: "105%",
+						top: "-180%",
+					}}
+				/>
+			</div>
 		</div>
 	);
 }
--- src/components/Textbox.tsx
+++ src/components/Textbox.tsx
@@ -1,53 +1,3 @@
-// interface TextboxProps {
-// 	divTop: string;
-// 	divLeft: string;
-// 	inputText: string;
-// 	inputType: string;
-// 	placeHolder: string;
-// 	inputWidth: string;
-// 	inputHeight: string;
-// }
-
-// const Textbox: React.FC<TextboxProps> = ({
-// 	divTop,
-// 	divLeft,
-// 	inputText,
-// 	inputType,
-// 	placeHolder,
-// 	inputWidth,
-// 	inputHeight,
-// }) => {
-// 	return (
-// 		<div className="textbox">
-// 			<div
-// 				style={{
-// 					position: "absolute",
-// 					top: divTop,
-// 					left: divLeft,
-// 					fontSize: "20px",
-// 				}}
-// 			>
-// 				<p className="font-bold">{inputText}</p>
-// 				<div>
-// 					<input
-// 						type={inputType}
-// 						placeholder={placeHolder}
-// 						className="absolute bg-[#0a5c0264] text-white px-4 py-2"
-// 						style={{
-// 							width: inputWidth,
-// 							height: inputHeight,
-// 							top: "calc(100vh / 25)",
-// 							wordWrap: "break-word",
-// 						}}
-// 					/>
-// 				</div>
-// 			</div>
-// 		</div>
-// 	);
-// };
-
-// export default Textbox;
-
 import React from "react";
 
 interface TextboxProps {
@@ -83,14 +33,15 @@ const Textbox: React.FC<TextboxProps> = ({
 				flexDirection: "column",
 			}}
 		>
-			<p className="font-bold">{inputText}</p>
-			<div style={{ position: "relative" }}>
+			<p>{inputText}</p>
+			<div className="py-0.5" style={{ position: "relative" }}>
 				<textarea
 					placeholder={placeHolder}
-					className="bg-[#0a5c0264] text-white px-4 py-2 resize-none"
+					className="bg-[#E2EBE2] text-black px-4 py-2 resize-none"
 					style={{
 						width: inputWidth,
 						height: inputHeight,
+						borderRadius: "10px",
 					}}
 					value={value}
 					onChange={onChange}
